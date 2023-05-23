09b239dd07ec180d8f8b282394fccefbd56e04d7
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-05-23 16:15:34 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,27 @@
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
-
-var cloud, cloudsGroup, cloudImage;
-
-
-
-var newImage;
-
+var cloud, cloudsGroup, cloudImage; // adicionar <<<<<<<<<<<<
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   trex_collided = loadAnimation("trex_collided.png");
-  
   groundImage = loadImage("ground2.png");
-  
-  cloudImage = loadImage("cloud.png");
- 
+  cloudImage = loadImage("cloud.png");// adicionar <<<<<<<<<<<<
 }
-
 function setup() {
   createCanvas(600, 200);
-
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
-  // trex.addAnimation("collided",trex_collided)
-  trex.scale = 0.5;
-  
+  trex.addAnimation("collided",trex_collided);
+  trex.scale = 0.5; 
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -4;
-  
   invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-  
+  invisibleGround.visible = false;  
 }
-
 function draw() {
-  background(180);
-  
-  
-  
+  background(180);  
   if(keyDown("space") && trex.y>=100) {
     trex.velocityY = -10;
   }
@@ -54,12 +35,12 @@ function draw() {
   trex.collide(invisibleGround);
   
   //gerar nuvens
-  spawnClouds();
+  spawnClouds(); // adicionar <<<<<<<<<<<<
   
   drawSprites();
 }
 
-function spawnClouds() {
+function spawnClouds() { // adicionar <<<<<<<<<<<<
   //escreva o código aqui para gerar as nuvens
   if (frameCount % 60 === 0) {
     cloud = createSprite(600,100,40,10);
@@ -67,7 +48,6 @@ function spawnClouds() {
     cloud.y = Math.round(random(10,60))
     cloud.scale = 0.4;
     cloud.velocityX = -3;
-    
     //ajustar a profundidade
     cloud.depth = trex.depth
     trex.depth = trex.depth + 1;
